829c6b156f0c77fcf1fb25a9b2631b05213ae852
Rafael de Almeida <rafa.amld3f@gmail.com>
2023-11-19 20:18:42 -0300

    adicionar funcionalidades

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const adicionarTarefa = document.querySelector('#novaTarefa');
 const caixaDeTarefas = document.querySelector('.caixaDeTarefas');
+const btnDelete = document.querySelector('.task button');
 
 adicionarTarefa.addEventListener('click', add);
 
-function add(){
+
+
+function add(e){
     const inputInserirTarefa = document.querySelector('#escreveTarefa').value;
-    event.preventDefault();
+    e.preventDefault();
 
     const itemDaLista = document.createElement('div');
     itemDaLista.classList.add('task');
@@ -15,11 +18,33 @@ function add(){
     
     const descricaoTarefa = document.createElement('p');
     descricaoTarefa.innerText = inputInserirTarefa;
-
+    
+    const botaoDelete = document.createElement('button');
+    
+    
+    const imagemLixeira = document.createElement('i');
+    imagemLixeira.classList.add('fa-solid');
+    imagemLixeira.classList.add('fa-trash');
+    
     
     caixaDeTarefas.appendChild(itemDaLista);
     itemDaLista.appendChild(input);
     itemDaLista.appendChild(descricaoTarefa);
+    itemDaLista.appendChild(botaoDelete);
+    botaoDelete.appendChild(imagemLixeira);
     
+    input.addEventListener('click', tarefaCompleta)
+    botaoDelete.addEventListener('click', deletaTarefa);
+    
+    document.querySelector('#escreveTarefa').value = '';
+
 }
 
+function deletaTarefa(e){
+    e.target.parentElement.parentElement.remove()
+}
+
+function tarefaCompleta(e) {
+    e.target.parentElement.classList.toggle('checkBoxMarcado')
+}
+/* de primeiro momento só deixei funcional. Da pr ver pela quantidade de código  que dá para deixar o código mais enxuto através de funções, mais pr frente a intenção é fazer isso, pois todolist é um bom tema para projeto de portifólio*/
